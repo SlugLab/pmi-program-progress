caf9e3d4cfc0aaa39dbb369b9345d65ac5bb72b7
victoryang00 <yangyiwei2000@gmail.com>
2023-04-06 11:17:28 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PMI Process Progress Monitor
+To make every 1million CPU instructions, and report the resident set size (RSS) of the process.
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by victoryang00 on 4/4/23.
+//
+#include <cerrno>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cxxopts.hpp>
+#include <range/v3/view.hpp>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
